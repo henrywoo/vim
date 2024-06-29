83dd3260d4ee8ac8fee48e5c1e09caa24c6753f7
Henry Wu <wufuheng@gmail.com>
2024-06-28 19:30:04 -0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 here = os.path.dirname(os.path.realpath(__file__))
 HAS_CUDA = os.system("nvidia-smi > /dev/null 2>&1") == 0
 
-VERSION = "0.0.1"
+VERSION = "0.0.1.dev0"
 DESCRIPTION = "VITVQGAN - VECTOR-QUANTIZED IMAGE MODELING WITH IMPROVED VQGAN"
 
 packages = [
